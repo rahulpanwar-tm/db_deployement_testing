bea19e79c0015a38eed2a63fc2696b70f256f6fd
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2018-10-22 11:32:13 +0530

    updated script

--- test_shell.sh
+++ test_shell.sh
@@ -16,7 +16,7 @@ for filename in /home/ttpl/.jenkins/workspace/Database_automation_testing/*.sql;
 # Array1=("${Array1[@]}" $(basename "$filename"))
 #Array1=("${Array1[@]}" "$filename")
 #echo "$filename"
- echo  "$(basename "$filename")"
+echo "${filename%.*}"
 
 done
 
