2069a545844fd81bb00f61091faa26b476db9240
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2018-10-22 11:24:35 +0530

    updated script

--- test_shell.sh
+++ test_shell.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+dest="/nas100/backups/servers/z/zebra/mysql.tgz"
+## get file name i.e. basename such as mysql.tgz
+tempfile="${dest##*/}"
+ 
+## display filename 
+echo "${tempfile%.*}"
+
 echo "Rahul Panwar"
