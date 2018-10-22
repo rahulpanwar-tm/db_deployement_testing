b3ef03a0896839fe9170a37c0ec99e6d4f50f8c6
Rahul Panwar <rahul.panwar@teramatrix.in>
2018-10-22 12:47:07 +0530

    update test.sh

--- test_shell.sh
+++ test_shell.sh
@@ -20,6 +20,16 @@ echo "${filename%.*}"
 
 done
 
+MAXCOUNT=10
+count=1
+
+while [ "$count" -le $MAXCOUNT ]; do
+ number[$count]=$RANDOM
+ let "count += 1"
+done
+
+echo "${number[*]}"
+
 echo "Rahul Panwar"
 
 
