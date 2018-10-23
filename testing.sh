6eaf2acb756166379d221c072162b7177d56eb79
Rahul Panwar <rahul.panwar@teramatrix.in>
2018-10-23 15:10:06 +0530

    update last time testing.sh

--- testing.sh
+++ testing.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #!/usr/bin/env bash
 
-echo "This script is about to run another script."
-MESSAGE="hello"
-IPADDRESS="192.168.1.122"
-USERID="developer"
-PASSWORD="admin@123"
-PORT="3306"
-echo "$MESSAGE"
+#echo "This script is about to run another script."
+#MESSAGE="hello"
+#IPADDRESS="192.168.1.122"
+#USERID="developer"
+#PASSWORD="admin@123"
+#PORT="3306"
+#echo "$MESSAGE"
 #bash ./deployement.sh
 
 . ./deployement.sh
 
-echo "This script has just run another script."
+#echo "This script has just run another script."
