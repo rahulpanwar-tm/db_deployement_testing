74ea730fb43912595ea70d1de45a39ae49bfb1a4
Rahul Panwar <rahul.panwar@teramatrix.in>
2018-10-23 15:11:30 +0530

    update21

--- testing.sh
+++ testing.sh
@@ -2,13 +2,13 @@
 #!/usr/bin/env bash
 
 #echo "This script is about to run another script."
-#MESSAGE="hello"
-#IPADDRESS="192.168.1.122"
-#USERID="developer"
-#PASSWORD="admin@123"
-#PORT="3306"
-#echo "$MESSAGE"
-#bash ./deployement.sh
+MESSAGE="hello"
+IPADDRESS="192.168.1.122"
+USERID="developer"
+PASSWORD="admin@123"
+PORT="3306"
+echo "$MESSAGE"
+bash ./deployement.sh
 
 . ./deployement.sh
 
