c8ac10df86e65889f190aaa24db5debfc5390f3c
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2018-10-23 16:44:14 +0530

    update all changes

--- testing.sh
+++ testing.sh
@@ -9,13 +9,21 @@
 # PORT="3306"
 # echo "$MESSAGE"
 
-MESSAGE="$IN_MESSAGE"
-IPADDRESS="$IN_IPADDRESS"
-USERID="$IN_USERID"
-PASSWORD="$IN_PASSWORD"
-PORT="$IN_PORT"
+# MESSAGE="$IN_MESSAGE"
+# IPADDRESS="$IN_IPADDRESS"
+# USERID="$IN_USERID"
+# PASSWORD="$IN_PASSWORD"
+# PORT="$IN_PORT"
+# echo "$MESSAGE"
+
+
+MESSAGE=$1
+IPADDRESS=$2
+USERID=$3
+PASSWORD=$4
+PORT="$5
 echo "$MESSAGE"
 
 . ./deployement.sh
 
-#echo "This script has just run another script."
+
