17e25036ef95e421eb694423bfdcd83e2516fb1a
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2018-10-24 00:19:09 +0530

    update341

--- final_deployement.sh
+++ final_deployement.sh
@@ -152,7 +152,7 @@ done
 echo "$MESSAGE"
 
 
-mysql -udeveloper -padmin@123 -c -h 192.168.1.122 -e "use deployement_status; select * from version_status;"
+mysql -udeveloper -padmin@123 -c -h 192.168.1.122 -e "use deployement_status; truncate  version_status;"
 
 
 
