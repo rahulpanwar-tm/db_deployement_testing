356dd90c5ce24f2e9a2215bd89f3e268adb45fc1
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2018-10-24 18:00:16 +0530

    update prod and globel server details

--- version_7.sql
+++
@@ -1,8 +0,0 @@
-use deployement_status;
--- Dumping structure for table deployement_status.organization
-DROP TABLE IF EXISTS `temp_organization`;
-CREATE TABLE IF NOT EXISTS `temp_organization` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `org_name` varchar(50) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
